7aa09c62508749df286f8e08a792b9dbf7ca8687
home <chr867423@gmail.com>
2023-05-11 08:53:20 +0900

    style : 변수 이름 수정

--- raw_data_multi.py
+++ raw_data_multi.py
@@ -11,12 +11,6 @@ import multiprocessing as mp
 import logging
 tqdm.pandas()
 
-'''
-전적 페이지로 이동(소환사 이름)
-소환사 이름으로 db 조회
-flask에서 정제한 df(횟수, 승률, KDA)를 json으로 변환해서 return
-'''
-
 riot_api_keys = private.riot_api_key_array
 
 # 티어별 유저 이름, 이름으로 puuid, puuid로 match id
@@ -60,11 +54,11 @@ def load_summoner_names_worker(worker_id):
             break
 
         print('load_summoner_names END', worker_id, len(name_set))
-        match_ids = set()
         name_lst = list(name_set)
         random.shuffle(name_lst)
 
-        for summoner_name in tqdm(name_lst[:50]):
+        match_set = set()
+        for summoner_name in tqdm(name_lst[:40]):
             while True:
                 index = 0
                 start = 1673485200  # 시즌 시작 Timestamp
@@ -79,7 +73,7 @@ def load_summoner_names_worker(worker_id):
                         url = f'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?startTime={start}&type=ranked&start={index}&count=100&api_key={api_key}'
                         res = requests.get(url).json()
                         index += 100
-                        match_ids.update(res)
+                        match_set.update(res)
                         if len(res) < 10:
                             break
 
@@ -94,9 +88,9 @@ def load_summoner_names_worker(worker_id):
 
                 break
 
-    match_ids = list(match_ids)
-    print('load_match_ids END', worker_id, len(match_ids))
-    return match_ids
+    match_list = list(match_set)
+    print('load_match_ids END', worker_id, len(match_list))
+    return match_list
 # 끝
 
 # match_id, matches, timeline 폼으로 만들기
@@ -107,7 +101,7 @@ def get_match_info_worker(args):
     tmp = set()
     random.shuffle(_match_ids)
 
-    for match_id in tqdm(_match_ids[:200]):  # 수정점
+    for match_id in tqdm(_match_ids[:500]):  # 수정점
         while True:
             time.sleep(1.25)
             try:
@@ -153,8 +147,8 @@ def insert_worker(args):
     match_info.progress_apply(lambda x: insert(x, conn), axis=1)
     conn.commit()
     conn.close()
-# insert
 
+# insert
 def insert(t, conn_):
     try:
         matches_json, timeline_json = conn_.escape_string(json.dumps(t.matches)), conn_.escape_string(json.dumps(t.timeline))
@@ -176,30 +170,35 @@ def main():
         f.write(formatted_time + " " + str(run_time))  # 런타임 값을 파일에 쓰기
     f.close()  # 파일 닫기
 
+    match_ids = set()
     with mp.Pool(processes=8) as pool:
         print('**load_summoner_names**  matches_timeline')
-        match_ids = set()
         for i, result in enumerate(tqdm(pool.imap(load_summoner_names_worker, range(8)))):
             match_ids.update(result)
 
     match_ids = list(match_ids)
     print('load_summoner_names  **get_match_info**', len(match_ids))
+    match_info_output = []
     with mp.Pool(processes=8) as pool:
         chunk_size = len(match_ids)
         chunks = [match_ids[i:i + chunk_size // 8] for i in range(0, len(match_ids), chunk_size // 8)]
-        match_info_output = []
         for i, res in enumerate(tqdm(pool.imap(get_match_info_worker, zip(chunks, range(8))), total=len(chunks))):
             match_info_output.append(res)
 
     merged_df = pd.concat(match_info_output)
     print("merged_df =", len(merged_df))
 
-    with mp.Pool(processes=12) as pool:
-        print('**insert**')
-        chunk_size = len(merged_df) // 12
-        chunks2 = [merged_df[i:i + chunk_size] for i in range(0, len(merged_df), chunk_size)]
-        for _ in tqdm(pool.imap(insert_worker, zip(chunks2, range(12))), total=len(chunks2)):
-            pass
+    sql_conn = mu.connect_mysql('my_db')
+    merged_df.progress_apply(lambda x: insert(x, sql_conn), axis=1)
+    sql_conn.commit()
+    sql_conn.close()
+
+    # with mp.Pool(processes=12) as pool:
+    #     print('**insert**')
+    #     chunk_size = len(merged_df) // 12
+    #     chunks2 = [merged_df[i:i + chunk_size] for i in range(0, len(merged_df), chunk_size)]
+    #     for _ in tqdm(pool.imap(insert_worker, zip(chunks2, range(12))), total=len(chunks2)):
+    #         pass
 
     print('done')
 
